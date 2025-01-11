5a29747f0ecf875021aee8bf4801939c0c51e947
oussamabartil <oussamabartil.04@gmail.com>
2025-01-11 23:12:08 +0100

    Implémenter la fonction générique de recherche par ID

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -6,6 +6,8 @@ const { ObjectId } = require('mongodb');
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
   // TODO: Implémenter une fonction générique de recherche par ID
+  const database = await db.connectMongo();
+  return await database.collection(collection).findOne({_id:new ObjectId(id)});
 }
 
 // Export des services
