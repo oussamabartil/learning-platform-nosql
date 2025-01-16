e865ea099dc2b62114ce7f3e354d259884565f19
oussamabartil <oussamabartil.04@gmail.com>
2025-01-16 21:40:16 +0100

    Ajout des fonctionnalités pour récupérer tous les étudiants et un étudiant par ID avec gestion du cache Redis

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -17,3 +17,38 @@ async function createStudent(req, res) {
   }
 }
 
+
+async function getAllStudents(req, res) {
+    try {
+      const students = await db.getDb().collection("students").find().toArray();
+      res.status(200).json(students);
+    } catch (error) {
+      console.error("Error getting students:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  
+  async function getStudentById(req, res) {
+    try {
+      const studentId = req.params.id;
+  
+      const cachedStudent = await redisService.getData(`student:${studentId}`);
+      if (cachedStudent) {
+        return res.status(200).json(cachedStudent);
+      }
+  
+      const student = await mongoService.findOneById(
+        db.getDb().collection("students"),
+        studentId
+      );
+      if (!student) {
+        return res.status(404).json({ error: "Student not found" });
+      }
+  
+      await redisService.cacheData(`student:${studentId}`, student, 3600); // Cache for 1 hour
+      res.status(200).json(student);
+    } catch (error) {
+      console.error("Error getting student by ID:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
